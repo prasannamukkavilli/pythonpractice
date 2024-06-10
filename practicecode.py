9c1f0c023840f797ee2ab77918636c3af0f54768
prasannamukkavilli <prasannamukkavilli@gmail.com>
2024-06-10 22:33:10 +0530

    Refracted the code with error free

--- practicecode.py
+++ practicecode.py
@@ -1,19 +1,6 @@
 # check a number if it is even or odd using conditional s
-
 x = 4
-
-
-
-if x%2=0:
+if x%2=="0":
     print("the given number is even")
-    else
+else:
     print("the given number is odd number")
-    
-x=5;
-y=x/2;
-if y=="0":
-    print("x is even number")
-    else
-    print("x is odd number")
-
-
